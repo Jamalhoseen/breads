bb89bb1dd1708418c5addc51149b58bcb406bb99
United States CA <jamalhoseen@Jamal-Hoseens-Air.local>
2022-03-13 18:48:22 -0700

    edit and update routes complete

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // DEPENDENCIES
 const express = require('express')
+const methodOverride = require('method-override')
 
 // CONFIGURATION
 require('dotenv').config()
@@ -15,6 +16,11 @@ app.use(express.static('public'))
 
 // MIDDLEWARE
 app.use(express.urlencoded({extended: true}))
+
+// MIDDLEWARE
+app.use(methodOverride('_method'))
+
+
 
 // ROUTES
 app.get('/', (req, res) => {
--- views/show.jsx
+++ views/show.jsx
@@ -1,12 +1,16 @@
 const React = require('react')
 const Default = require('./layouts/Default')
 
-function Show ({bread}) {
+function Show ({bread, index}) {
     // Confirm we are getting our bread data in the terminal.
     // console.log(bread.name)
       return (
         <Default>
   <h3>{bread.name}</h3>
+  <form action={`/breads/${index}?_method=DELETE`} method="POST">
+  <input type='submit' value="DELETE"/>
+  </form>
+  <a href={`/breads/${index}/edit`}><button>Edit</button></a>
   <p>
     and it
     {
@@ -21,5 +25,8 @@ function Show ({bread}) {
 </Default>
       )
   }
+
+
+
 
 module.exports = Show
